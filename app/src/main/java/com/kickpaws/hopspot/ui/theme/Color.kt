999215468683b65fa52a8064374e47f744ba0f35
CKnuchel <christoph.knuchel@gmail.com>
2026-01-30 13:03:50 +0100

    CHANGED: Changed the colors to beer themed values

--- app/src/main/java/com/kickpaws/hopspot/ui/theme/Color.kt
+++ app/src/main/java/com/kickpaws/hopspot/ui/theme/Color.kt
@@ -2,10 +2,21 @@ package com.kickpaws.hopspot.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary - Bier/Amber Töne
+val Amber700 = Color(0xFFFF8F00)
+val Amber500 = Color(0xFFFFB300)
+val Amber300 = Color(0xFFFFCA28)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Secondary - Holz/Braun Töne
+val Brown700 = Color(0xFF5D4037)
+val Brown500 = Color(0xFF795548)
+val Brown300 = Color(0xFFA1887F)
+
+// Background/Surface
+val Cream = Color(0xFFFFF8E1)
+val CreamDark = Color(0xFFFFF3CD)
+
+// Neutrals
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF1C1B1F)
+val BlackLight = Color(0xFF49454F)
