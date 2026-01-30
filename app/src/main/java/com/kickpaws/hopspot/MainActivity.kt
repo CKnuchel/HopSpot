4b73ab99193d0fb48dd3433d8eae36aad985fca2
CKnuchel <christoph.knuchel@gmail.com>
2026-01-30 17:45:38 +0100

    ADDED: Defined BottomNavigationBar in MainActivity.kt

--- app/src/main/java/com/kickpaws/hopspot/MainActivity.kt
+++ app/src/main/java/com/kickpaws/hopspot/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.kickpaws.hopspot.ui.components.BottomNavigationBar
 import com.kickpaws.hopspot.ui.navigation.HopSpotNavGraph
+import com.kickpaws.hopspot.ui.navigation.Route
 import com.kickpaws.hopspot.ui.theme.HopSpotTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,15 +23,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HopSpotTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
-                    HopSpotNavGraph(navController = navController)
+                val navController = rememberNavController()
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
+
+                val showBottomBar = currentRoute in listOf(
+                    Route.Map.route,
+                    Route.BenchList.route,
+                    Route.Visits.route,
+                    Route.Profile.route
+                )
+
+                Scaffold(
+                    bottomBar = {
+                        if (showBottomBar) {
+                            BottomNavigationBar(navController = navController)
+                        }
+                    }
+                ) { innerPadding ->
+                    HopSpotNavGraph(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
     }
 }
-
