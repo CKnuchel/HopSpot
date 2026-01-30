4e73d014557c3b26426b2f92b405c3a04005fbf8
CKnuchel <christoph.knuchel@gmail.com>
2026-01-30 13:00:09 +0100

    Improved View

--- app/src/main/java/com/kickpaws/hopspot/MainActivity.kt
+++ app/src/main/java/com/kickpaws/hopspot/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kickpaws.hopspot.ui.theme.HopSpotTheme
@@ -23,9 +25,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HopSpotTheme {
-                Surface {
-                    Box {
-                        Text("HopSpot 🍺")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "HopSpot 🍺",
+                            style = MaterialTheme.typography.headlineLarge
+                        )
                     }
                 }
             }
