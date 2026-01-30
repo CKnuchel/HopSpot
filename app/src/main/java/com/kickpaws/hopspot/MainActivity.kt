4b9220580fb584edb800686be1cb1acd5e5e5394
CKnuchel <christoph.knuchel@gmail.com>
2026-01-30 17:21:12 +0100

    CHANGED: Defined NavGraph in MainActivity.kt

--- app/src/main/java/com/kickpaws/hopspot/MainActivity.kt
+++ app/src/main/java/com/kickpaws/hopspot/MainActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.kickpaws.hopspot.ui.navigation.HopSpotNavGraph
 import com.kickpaws.hopspot.ui.theme.HopSpotTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -29,15 +24,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = "HopSpot 🍺",
-                            style = MaterialTheme.typography.headlineLarge
-                        )
-                    }
+                    val navController = rememberNavController()
+                    HopSpotNavGraph(navController = navController)
                 }
             }
         }
